679fd3883a1eb06a4b5094f69addd139506bd8e7
John Nico M. Edisan <nicxsician@gmail.com>
2024-02-18 07:20:35 +0800

    Create updateLabelText.py

---
+++ updateLabelText.py
@@ -0,0 +1,19 @@
+from customtkinter import *
+
+app = CTk()
+app.geometry("500x400")
+
+count = 0
+
+def click_handler():
+    global count
+    count += 1
+    label.configure(text=f"You've clicked the button {count} times")
+
+label = CTkLabel(master=app, text="You've Click the button 0 times")
+btn = CTkButton(master=app, text="Click Me", command=click_handler)
+
+label.pack(anchor="s", expand=True, pady=10)
+btn.pack(anchor="n", expand=True, pady=10)
+
+app.mainloop()
