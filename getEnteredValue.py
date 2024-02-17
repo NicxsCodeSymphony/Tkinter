e84bf15d2d6365051730b3ddaaa3d9e9439c81b6
John Nico M. Edisan <nicxsician@gmail.com>
2024-02-18 07:15:33 +0800

    Create getEnteredValue.py

---
+++ getEnteredValue.py
@@ -0,0 +1,15 @@
+from customtkinter import *
+
+app = CTk()
+app.geometry("500x400")
+
+def click_handler():
+    print(f'Entered Value: {entry.get()}')
+
+entry = CTkEntry(master=app, placeholder_text="Type Anything")
+btn = CTkButton(master=app, text="SUBMIT", command=click_handler)
+
+entry.pack(anchor="s", expand=True, pady=10)
+btn.pack(anchor="n", expand=True)
+
+app.mainloop()
